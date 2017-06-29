ac8200d20f8f5f2f6b7fe0317e68de2c879c660d
nandehutuzn <120902259@qq.com>
2017-06-29 23:07:06 +0800

    1.1基础知识

---
+++ AlgorithmSolution/ZN.Core.Algorithm.SimpleBasis/Exercise.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZN.Core.Algorithm.SimpleBasis
+{
+    /// <summary>
+    /// 第一章练习
+    /// </summary>
+    public class Exercise
+    {
+        /// <summary>
+        /// 整型转换成二进制字符串
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static string IntToBinaryString(int num)
+        {
+            string s = "";
+            for (int i = num; i > 0; i /= 2)
+                s = i % 2 + s;
+            return s;
+        }
+
+        /// <summary>
+        /// 返回大小为参数m的数组，其中第i个元素的值为整数i在参数数组中出现的次数
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static int[] Histogram(int[] array, int m)
+        {
+            int[] result = new int[m];
+            Array.Sort(array);//先对该数组排序
+            for (int i = 0; i < m; i++)
+            {
+                int index = Foreword.Rank(i + 1, array);//用二分查找法找出该数在数组中的位置
+                if (index == -1) result[i] = 0;
+                else result[i] = GetCountInSortedArray(array, index);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 在已排好序的数组中查询与索引为index相同值的数的个数
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static int GetCountInSortedArray(int[] array, int index)
+        {
+            int count = 0;
+            for (int i = index; i < array.Length; i++)
+            {
+                if (array[index] == array[i]) count++;
+                else break;
+            }
+            for (int i = index - 1; i > 0; i--)
+            {
+                if (array[index] == array[i]) count++;
+                else break;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// a*b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int MyStery(int a, int b)
+        {
+            if (b == 0) return 0;
+            if (b % 2 == 0) return MyStery(a + a, b / 2);
+            return MyStery(a + a, b / 2) + a;
+        }
+
+        /// <summary>
+        /// 斐波那契数列
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        /// <summary>
+        /// 斐波那契数列  改进版
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long FibonacciPro(int n)
+        {
+            if (n <= 0)
+                return n;
+            long a = 1;
+            long b = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                b = a + b;
+                a = b - a;
+            }
+            return b;
+        }
+    }
+}
---
+++ AlgorithmSolution/ZN.Core.Algorithm.SimpleBasis/Foreword.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZN.Core.Algorithm.SimpleBasis
+{
+    /// <summary>
+    /// by zhangning   2017.6.29
+    /// </summary>
+    public class Foreword
+    {
+        /// <summary>
+        /// 求最大公约数
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public static int GetMaxCommonDivisor(int p, int q)
+        {
+            if (q == 0) return p;
+            return GetMaxCommonDivisor(q, p % q);
+        }
+
+        /// <summary>
+        /// 二分查找法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="a">有序数组</param>
+        /// <returns></returns>
+        public static int Rank(int key, int[] a)
+        {
+            int low = 0;
+            int high = a.Length - 1;
+            while (low <= high)
+            {//被查找的键要么不存在，要么必然存在于 a[low...high]中
+                int mid = low + (high - low) / 2;
+                if (key < a[mid]) high = mid - 1;
+                else if (key > a[mid]) low = mid + 1;
+                else return mid;
+            }
+            return -1;
+        }
+    }
+}
